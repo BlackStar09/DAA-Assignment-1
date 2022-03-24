a47a5ced081be2ca7912799ca248e5d0f4379ab7
BlackStar09 <73814092+BlackStar09@users.noreply.github.com>
2022-03-24 16:33:01 +0530

    small home page change

--- src/Home/StudentCard.js
+++ src/Home/StudentCard.js
@@ -21,19 +21,6 @@ class StudentCard extends React.Component {
           justifyContent: "center"
         }}
       >
-        <img
-          src={ProfielImg}
-          style={{
-            height: 100,
-            width: 100,
-            borderRadius: 50,
-            alignSelf: "center",
-            alignItems: "center",
-            alignContent: "center",
-            marginTop: 10
-          }}
-          alt="Img"
-        />
         <h1 style={{ textAlign: "center", color: "white" }}>
           {this.props.item.name}
         </h1>
